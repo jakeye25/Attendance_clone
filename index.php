fad88c28f13cdeeb614f73546af9e35444e3f77d
jayyedata <jayyeucla@gmail.com>
2023-03-10 13:33:00 -0800

    update index

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     $title = 'Index';
     require_once 'includes/header.php'; ?>
 
-<h1>Hello, world!</h1>
+
+    <h1>Registration for IT Conference</h1>
+
 
 <?php require_once 'includes/footer.php'; ?>
