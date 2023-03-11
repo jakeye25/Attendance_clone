807fd2a77077e519e05cdbf5828c39716d8f7d5e
jayyedata <jayyeucla@gmail.com>
2023-03-10 16:31:31 -0800

    form setup

--- includes/header.php
+++ includes/header.php
@@ -5,11 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Attendance - <?php echo $title ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="css/site.css" />
 
 </head>
   <body>
     <div class="container">
-
-
--- index.php
+++ index.php
@@ -1,10 +1,52 @@
-
 <?php
-    $title = 'Index';
-    require_once 'includes/header.php'; ?>
+$title = 'Index';
+require_once 'includes/header.php'; ?>
 
 
-    <h1>Registration for IT Conference</h1>
+<h1 class="text-center">Registration for IT Conference</h1>
 
-
+<form>
+    <div class="mb-3">
+        <label for="firstname" class="form-label">First Name</label>
+        <input type="text" class="form-control" id="firstname">
+    </div>
+    <div class="mb-3">
+        <label for="lastname" class="form-label">Last Name</label>
+        <input type="text" class="form-control" id="lastname">
+    </div>
+    <div class="mb-3">
+        <label for="dob" class="form-label">Date of Birth</label>
+        <input type="text" class="form-control" id="dob">
+    </div>
+    <div class="mb-3">
+        <label for="specialty" class="form-label">Area of Expertise</label>
+        <select class="form-select" id="specialty">
+            <option selected>Open this select menu</option>
+            <option value="1">Database Admin</option>
+            <option value="2">Software Developer</option>
+            <option value="3">Web Administrator</option>
+            <option value="3">Other</option>
+        </select>
+    </div>
+    <div class="mb-3">
+        <label for="phone" class="form-label">Contact Number</label>
+        <input type="text" class="form-control" id="phone">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<br/>
+<br/>
 <?php require_once 'includes/footer.php'; ?>
