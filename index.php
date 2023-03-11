b7e318c3bcf2dd9a248a245e1f5c83ddc04bce90
jayyedata <jayyeucla@gmail.com>
2023-03-10 19:47:29 -0800

    update get form

--- index.php
+++ index.php
@@ -8,19 +8,19 @@ require_once 'includes/header.php'; ?>
 <form method="get" action="success.php">
     <div class="mb-3">
         <label for="firstname" class="form-label">First Name</label>
-        <input type="text" class="form-control" id="firstname">
+        <input type="text" class="form-control" id="firstname" name="firstname">
     </div>
     <div class="mb-3">
         <label for="lastname" class="form-label">Last Name</label>
-        <input type="text" class="form-control" id="lastname">
+        <input type="text" class="form-control" id="lastname" name="lastname">
     </div>
     <div class="mb-3">
         <label for="dob" class="form-label">Date of Birth</label>
-        <input type="text" class="form-control" id="dob">
+        <input type="text" class="form-control" id="dob" name="dob">
     </div>
     <div class="mb-3">
         <label for="specialty" class="form-label">Area of Expertise</label>
-        <select class="form-select" id="specialty">
+        <select class="form-select" id="specialty" name="specialty">
             <option selected>Open this select menu</option>
             <option value="1">Database Admin</option>
             <option value="2">Software Developer</option>
@@ -30,22 +30,22 @@ require_once 'includes/header.php'; ?>
     </div>
     <div class="mb-3">
         <label for="phone" class="form-label">Contact Number</label>
-        <input type="text" class="form-control" id="phone">
+        <input type="text" class="form-control" id="phone" name="phone">
     </div>
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
     </div>
     <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <input type="password" class="form-control" id="password" name="password">
     </div>
     <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary btn-block" name="submit">Submit</button>
 </form>
 <br/>
 <br/>
--- success.php
+++ success.php
@@ -1,9 +1,30 @@
 <?php
-$title = 'Index';
+$title = 'Success';
 require_once 'includes/header.php'; ?>
 
 
+<h1 class="text-center text-success">You Have Been Registered!</h1>
 
-<br/>
-<br/>
+<div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+    </div>
+</div>
+
+<?php
+echo $_GET['firstname'];
+echo $_GET['lasstname'];
+echo $_GET['dob'];
+echo $_GET['specialty'];
+echo $_GET['email'];
+echo $_GET['phone'];
+
+
+?>
+<br />
+<br />
 <?php require_once 'includes/footer.php'; ?>
