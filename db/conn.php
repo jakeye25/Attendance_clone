06f781c52f5a751e82507252347c870b9f2d5284
jayyedata <jayyeucla@gmail.com>
2023-03-11 17:40:03 -0800

    add database

--- db/conn.php
+++ db/conn.php
@@ -1,3 +1,17 @@
 <?php
-    
+    $host = '127.0.0.1';
+    $db = 'attendance';
+    $user='root';
+    $pass='';
+    $charset = 'utf8mb4';
+    //data source name
+    $dsn =  "mysql:host=$host;dbname=$db;charset=$charset";
+
+    try{
+        $pdo = new PDO($dsn, $user, $pass);
+        echo 'Hello Database';
+    } catch(PDOException $e){
+        throw new PDOException($e->getMessage());
+    }
+
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 $title = 'Index';
-require_once 'includes/header.php'; ?>
+require_once 'includes/header.php';
+require_once 'db/conn.php';
+?>
 
 
 <h1 class="text-center">Registration for IT Conference</h1>
