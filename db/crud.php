428a0e337e8a4807f9fdc6b8b76ffd74674e155f
jayyedata <jayyeucla@gmail.com>
2023-03-12 21:51:07 -0700

    update edit function

--- db/crud.php
+++ db/crud.php
@@ -1,60 +1,85 @@
 <?php
-    class crud{
-        //private database object
-        private $db;
+class crud
+{
+    //private database object
+    private $db;
 
-        //constructor to initialize private variable to the databasse connection
-        function __construct($conn){
-            $this->db = $conn;
-        }
+    //constructor to initialize private variable to the databasse connection
+    function __construct($conn)
+    {
+        $this->db = $conn;
+    }
 
-        public function insertAttendees($fname, $lname, $dob, $email, $contact, $specialty){
-            try {
-                //define sqql statement to be exxecuted
-                $sql = "INSERT INTO attendee (firstname,lastname,dateofbirth,emailaddress,contactnumber,specialty_id) VALUES (:fname,:lname,:dob,:email,:contact,:specialty)";
-                //prepare the sql statement for execution
-                $stmt = $this->db->prepare($sql);
-                //bind all placeholders to the actual values
-                $stmt->bindparam(':fname',$fname);
-                $stmt->bindparam(':lname',$lname);
-                $stmt->bindparam(':dob',$dob);
-                $stmt->bindparam(':email',$email);
-                $stmt->bindparam(':contact',$contact);
-                $stmt->bindparam(':specialty',$specialty);
-
-                $stmt->execute();
-                return true;
-
-            } catch(PDOException $e) {
-                echo $e->getMessage();
-                return false;
-            }
-        }
-
-        public function getAttendees(){
-            $sql = "SELECT * FROM `attendee` a inner join specialities s on a.specialty_id = s.specialty_id;";
-            $result = $this->db->query($sql);
-            return $result;
-        }
-
-        public function getAttendeeDetail($id){
-            $sql = "select * from attendee a inner join specialities s on a.specialty_id = s.specialty_id
-            where attendee_id = :id";
+    public function insertAttendees($fname, $lname, $dob, $email, $contact, $specialty)
+    {
+        try {
+            //define sqql statement to be exxecuted
+            $sql = "INSERT INTO attendee (firstname,lastname,dateofbirth,emailaddress,contactnumber,specialty_id) VALUES (:fname,:lname,:dob,:email,:contact,:specialty)";
+            //prepare the sql statement for execution
             $stmt = $this->db->prepare($sql);
-            $stmt->bindparam(':id', $id);
+            //bind all placeholders to the actual values
+            $stmt->bindparam(':fname', $fname);
+            $stmt->bindparam(':lname', $lname);
+            $stmt->bindparam(':dob', $dob);
+            $stmt->bindparam(':email', $email);
+            $stmt->bindparam(':contact', $contact);
+            $stmt->bindparam(':specialty', $specialty);
+
             $stmt->execute();
-            $result= $stmt->fetch();
-            return $result;
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
         }
+    }
 
-        public function getSpecialties(){
-            $sql = "SELECT * FROM `specialities`;";
-            $result = $this->db->query($sql);
-            return $result;
+    public function editAttendee($id, $fname, $lname, $dob, $email, $contact, $specialty){
+        try {
+            $sql = "UPDATE `attendee` SET `firstname`=:fname,`lastname`=:lname,`dateofbirth`=:dob,
+            `emailaddress`=:email,`contactnumber`=:contact,`specialty_id`=:specialty WHERE attendee_id = :id";
+
+            $stmt = $this->db->prepare($sql);
+            //bind all placeholders to the actual values
+            $stmt->bindparam(':id', $id);
+            $stmt->bindparam(':fname', $fname);
+            $stmt->bindparam(':lname', $lname);
+            $stmt->bindparam(':dob', $dob);
+            $stmt->bindparam(':email', $email);
+            $stmt->bindparam(':contact', $contact);
+            $stmt->bindparam(':specialty', $specialty);
+
+            $stmt->execute();
+            return true;
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
         }
-
 
     }
 
+    public function getAttendees()
+    {
+        $sql = "SELECT * FROM `attendee` a inner join specialities s on a.specialty_id = s.specialty_id;";
+        $result = $this->db->query($sql);
+        return $result;
+    }
 
-?>
+    public function getAttendeeDetail($id)
+    {
+        $sql = "select * from attendee a inner join specialities s on a.specialty_id = s.specialty_id
+            where attendee_id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindparam(':id', $id);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        return $result;
+    }
+
+    public function getSpecialties()
+    {
+        $sql = "SELECT * FROM `specialities`;";
+        $result = $this->db->query($sql);
+        return $result;
+    }
+}
--- edit.php
+++ edit.php
@@ -7,43 +7,58 @@ require_once 'db/conn.php';
 
 $results = $crud->getSpecialties();
 
+if(!isset($_GET['id'])){
+    echo 'error';
+} else {
+    $id = $_GET['id'];
+    $attendee = $crud->getAttendeeDetail($id);
+
 ?>
 
 
 <h1 class="text-center">Edit Record</h1>
 
-<form method="post" action="success.php">
+<form method="post" action="editpost.php">
+
+    <input type="hidden" name="id" value="<?php echo $attendee['attendee_id'] ?>"/>>
+
     <div class="mb-3">
         <label for="firstname" class="form-label">First Name</label>
-        <input type="text" class="form-control" id="firstname" name="firstname">
+        <input type="text" class="form-control" value="<?php echo $attendee['firstname']; ?>" id="firstname" name="firstname">
     </div>
     <div class="mb-3">
         <label for="lastname" class="form-label">Last Name</label>
-        <input type="text" class="form-control" id="lastname" name="lastname">
+        <input type="text" class="form-control" value="<?php echo $attendee['lastname']; ?>" id="lastname" name="lastname">
     </div>
     <div class="mb-3">
         <label for="dob" class="form-label">Date of Birth</label>
-        <input type="text" class="form-control" id="dob" name="dob">
+        <input type="text" class="form-control" value="<?php echo $attendee['dateofbirth']; ?>" id="dob" name="dob">
     </div>
     <div class="mb-3">
         <label for="specialty" class="form-label">Area of Expertise</label>
         <select class="form-select" id="specialty" name="specialty">
             <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) { ?>
-                <option value="<?php echo $r['specialty_id']; ?>"><?php echo $r['name']; ?></option>
+                <option value="<?php echo $r['specialty_id']; ?>" <?php if($r['specialty_id'] ==
+                $attendee['specialty_id']) echo 'selected' ?>>
+                <?php echo $r['name']; ?>
+            </option>
             <?php } ?>
         </select>
     </div>
     <div class="mb-3">
         <label for="contact" class="form-label">Contact Number</label>
-        <input type="text" class="form-control" id="contact" name="contact">
+        <input type="text" class="form-control" value="<?php echo $attendee['contactnumber']; ?>" id="contact" name="contact">
     </div>
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" value="<?php echo $attendee['emailaddress']; ?>">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
     </div>
     <button type="submit" class="btn btn-success btn-block" name="submit">Save Changes</button>
 </form>
+
+<?php } ?>
+
 <br/>
 <br/>
 <?php require_once 'includes/footer.php'; ?>
