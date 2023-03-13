6a4d019a0261e6a1f63187d74b60076e9e1d7a8f
jayyedata <jayyeucla@gmail.com>
2023-03-12 17:40:44 -0700

    update view a record

--- viewrecords.php
+++ viewrecords.php
@@ -13,9 +13,9 @@ $results = $crud->getAttendees();
         <th>#</th>
         <th>First Name</th>
         <th>Last Name</th>
-        <th>Date of Birth</th>
+        <!-- <th>Date of Birth</th>
         <th>Email Address</th>
-        <th>Contact Number</th>
+        <th>Contact Number</th> -->
         <th>Specialty</th>
         <th>Actions</th>
     </tr>
@@ -26,11 +26,11 @@ $results = $crud->getAttendees();
         <td><?php echo $r['attendee_id']; ?></td>
         <td><?php echo $r['firstname']; ?></td>
         <td><?php echo $r['lastname']; ?></td>
-        <td><?php echo $r['dateofbirth']; ?></td>
-        <td><?php echo $r['emailaddress']; ?></td>
-        <td><?php echo $r['contactnumber']; ?></td>
+        <!-- <td><?php //echo $r['dateofbirth']; ?></td>
+        <td><?php //echo $r['emailaddress']; ?></td>
+        <td><?php //echo $r['contactnumber']; ?></td> -->
         <td><?php echo $r['name']; ?></td>
-        <td><a href="view.php" class="btn btn-primary">View</a></td>
+        <td><a href="view.php?id=<?php echo $r['attendee_id']; ?>" class="btn btn-primary">View</a></td>
     </tr>
         <?php } ?>
     </tr>
