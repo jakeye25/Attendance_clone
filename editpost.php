4d427f0417f2d7ff35fb921d31495a1c87693782
jayyedata <jayyeucla@gmail.com>
2023-09-24 17:26:00 -0700

    update mms

--- editpost.php
+++ editpost.php
@@ -8,8 +8,9 @@ if(isset($_POST['submit'])){
     $id = $_POST['id'];
 
     $fname = $_POST['firstname'];
-    
+
     $lname = $_POST['lastname'];
+    
     $dob = $_POST['dob'];
     $email = $_POST['email'];
     $contact = $_POST['contact'];
