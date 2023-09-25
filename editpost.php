4f4ec6a315070d5cf0784d0ee09dca12730f6afd
jayyedata <jayyeucla@gmail.com>
2023-09-24 17:25:42 -0700

    update mms

--- editpost.php
+++ editpost.php
@@ -6,6 +6,7 @@ if(isset($_POST['submit'])){
 
     //extract values from the $_POST array
     $id = $_POST['id'];
+    
     $fname = $_POST['firstname'];
     $lname = $_POST['lastname'];
     $dob = $_POST['dob'];
@@ -14,7 +15,7 @@ if(isset($_POST['submit'])){
     $specialty = $_POST['specialty'];
 
     //call crud function
-    
+
     $result = $crud->editAttendee($id, $fname, $lname, $dob, $email, $contact, $specialty);
 
     //redirect to index.php
