3e768c8743e079c088d7432c8e68e2b3fb9d2f5d
jayyedata <jayyeucla@gmail.com>
2023-09-26 15:49:13 -0700

    update mms

--- edit.php
+++ edit.php
@@ -30,12 +30,13 @@ if(!isset($_GET['id'])){
     </div>
 
     <div class="mb-3">
-        
+
         <label for="dob" class="form-label">Date of Birth</label>
         <input type="text" class="form-control" value="<?php echo $attendee['dateofbirth']; ?>" id="dob" name="dob">
     </div>
 
     <div class="mb-3">
+        
         <label for="specialty" class="form-label">Area of Expertise</label>
         <select class="form-select" id="specialty" name="specialty">
             <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) { ?>
