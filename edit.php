dcf6a4e1968543275d376ebd4fd194cf95aa1b25
jayyedata <jayyeucla@gmail.com>
2023-10-09 20:45:02 -0700

    update mms

--- edit.php
+++ edit.php
@@ -12,7 +12,6 @@ if(!isset($_GET['id'])){
     $id = $_GET['id'];
     $attendee = $crud->getAttendeeDetail($id);
 ?>
-
 <h1 class="text-center">Edit Record</h1>
 <form method="post" action="editpost.php">
     <input type="hidden" name="id" value="<?php echo $attendee['attendee_id'] ?>"/>
