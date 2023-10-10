6db86da1dd0ac0e4560d2e5daa1820f5f24488a7
jayyedata <jayyeucla@gmail.com>
2023-10-09 20:43:43 -0700

    update mms

--- edit.php
+++ edit.php
@@ -31,7 +31,6 @@ if(!isset($_GET['id'])){
         <input type="text" class="form-control" value="<?php echo $attendee['dateofbirth']; ?>" id="dob" name="dob">
 
     </div>
-
     <div class="mb-3">
         <label for="specialty" class="form-label">Area of Expertise</label>
         <select class="form-select" id="specialty" name="specialty">
