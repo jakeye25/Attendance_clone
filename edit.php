e2adecad2edde60eb9c4933052d1e26597c0df12
jayyedata <jayyeucla@gmail.com>
2023-10-31 22:32:59 -0700

    update mms

--- edit.php
+++ edit.php
@@ -22,6 +22,7 @@ if(!isset($_GET['id'])){
 <h1 class="text-center">Edit Record</h1>
 
 <form method="post" action="editpost.php">
+    
     <input type="hidden" name="id" value="<?php echo $attendee['attendee_id'] ?>"/>
     <div class="mb-3">
         <label for="firstname" class="form-label">First Name</label>
