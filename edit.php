da1984a936831edc52249245ddde3c283739870a
jayyedata <jayyeucla@gmail.com>
2023-11-06 20:13:46 -0800

    update mms

--- edit.php
+++ edit.php
@@ -42,8 +42,9 @@ if(!isset($_GET['id'])){
     </div>
 
     <div class="mb-3">
-        
+
         <label for="dob" class="form-label">Date of Birth</label>
+        
         <input type="text" class="form-control" value="<?php echo $attendee['dateofbirth']; ?>" id="dob" name="dob">
     </div>
     <div class="mb-3">
