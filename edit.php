03028bacfd23b51fe59e3e284630c2eb5f4c8e87
jayyedata <jayyeucla@gmail.com>
2023-11-13 20:42:14 -0800

    update mms

--- edit.php
+++ edit.php
@@ -58,6 +58,7 @@ if(!isset($_GET['id'])){
             <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) { ?>
 
                 <option value="<?php echo $r['specialty_id']; ?>" <?php if($r['specialty_id'] ==
+
                 $attendee['specialty_id']) echo 'selected' ?>>
                 <?php echo $r['name']; ?>
             </option>
