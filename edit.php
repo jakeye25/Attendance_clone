f4ad3959dafa8d932be09fbe83aea95781b2c65a
jayyedata <jayyeucla@gmail.com>
2023-11-13 20:42:24 -0800

    update mms

--- edit.php
+++ edit.php
@@ -60,6 +60,7 @@ if(!isset($_GET['id'])){
                 <option value="<?php echo $r['specialty_id']; ?>" <?php if($r['specialty_id'] ==
 
                 $attendee['specialty_id']) echo 'selected' ?>>
+
                 <?php echo $r['name']; ?>
             </option>
             <?php } ?>
