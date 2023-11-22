ce6f57aea8f35d66aa9197304f3543c2ec610104
jayyedata <jayyeucla@gmail.com>
2023-11-21 17:51:19 -0800

    update mms

--- edit.php
+++ edit.php
@@ -72,8 +72,9 @@ if(!isset($_GET['id'])){
     </div>
 
     <div class="mb-3">
-        
+
         <label for="contact" class="form-label">Contact Number</label>
+
         <input type="text" class="form-control" value="<?php echo $attendee['contactnumber']; ?>" id="contact" name="contact">
     </div>
     <div class="mb-3">
