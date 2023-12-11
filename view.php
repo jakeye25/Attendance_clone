8544eab7a37088d617d572ca8aeaa09d283ac3dc
jayyedata <jayyeucla@gmail.com>
2023-12-11 10:45:39 -0800

    update mms

--- view.php
+++ view.php
@@ -15,6 +15,7 @@ if (!isset($_GET['id'])) {
 ?>
 
     <div class="card" style="width: 18rem;">
+    
         <div class="card-body">
             <h5 class="card-title"><?php echo $result['firstname'] . ' ' . $result['lastname']; ?></h5>
             <h6 class="card-subtitle mb-2 text-muted">
