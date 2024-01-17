2b5ffec3b7cf2ce9829a01cfd6a8018a4590af56
jayyedata <jayyeucla@gmail.com>
2024-01-16 21:46:41 -0800

    update mms

--- edit.php
+++ edit.php
@@ -54,7 +54,6 @@ if(!isset($_GET['id'])){
         <label for="specialty" class="form-label">Area of Expertise</label>
 
         <select class="form-select" id="specialty" name="specialty">
-
             <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) { ?>
                 <option value="<?php echo $r['specialty_id']; ?>" <?php if($r['specialty_id'] ==
                 $attendee['specialty_id']) echo 'selected' ?>>
