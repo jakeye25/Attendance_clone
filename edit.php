ae872e59833a6752863ceb3b9acc78cc62033786
jayyedata <jayyeucla@gmail.com>
2024-01-16 21:46:31 -0800

    update mms

--- edit.php
+++ edit.php
@@ -56,7 +56,6 @@ if(!isset($_GET['id'])){
         <select class="form-select" id="specialty" name="specialty">
 
             <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) { ?>
-
                 <option value="<?php echo $r['specialty_id']; ?>" <?php if($r['specialty_id'] ==
                 $attendee['specialty_id']) echo 'selected' ?>>
                 <?php echo $r['name']; ?>
