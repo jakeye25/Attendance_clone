182b87e28898bbc1593e32d89aa3a5af76ba81d7
jayyedata <jayyeucla@gmail.com>
2024-01-22 21:55:20 -0800

    update mms

--- edit.php
+++ edit.php
@@ -68,7 +68,6 @@ if(!isset($_GET['id'])){
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email address</label>
         <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" value="<?php echo $attendee['emailaddress']; ?>">
-
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
 
     </div>
