4ff763b998c79e43b988f3080be6470347e02b41
jayyedata <jayyeucla@gmail.com>
2024-01-22 21:55:31 -0800

    update mms

--- edit.php
+++ edit.php
@@ -71,7 +71,6 @@ if(!isset($_GET['id'])){
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
 
     </div>
-
     <a href="viewrecords.php" class="btn btn-default" name="submit">Back To List</a>
 
     <br/>
