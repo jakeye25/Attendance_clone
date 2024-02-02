1c42464cf508ba47be2427f68c99617cd1a759db
jayyedata <jayyeucla@gmail.com>
2024-02-02 11:13:17 -0800

    update mms

--- editpost.php
+++ editpost.php
@@ -11,7 +11,6 @@ if(isset($_POST['submit'])){
     $dob = $_POST['dob'];
     $email = $_POST['email'];
     $contact = $_POST['contact'];
-
     $specialty = $_POST['specialty'];
 
     //call crud function
