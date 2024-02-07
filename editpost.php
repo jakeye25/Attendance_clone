ae66c2571fc83d7d9ecc27378cde7ae20b1aa471
jayyedata <jayyeucla@gmail.com>
2024-02-07 10:27:08 -0800

    update mms

--- editpost.php
+++ editpost.php
@@ -19,7 +19,7 @@ if(isset($_POST['submit'])){
         header("Location: viewrecords.php");
     } else {
     include 'includes/errormessage.php';
-}
+    }
 } else {
     include 'includes/errormessage.php';
 }
