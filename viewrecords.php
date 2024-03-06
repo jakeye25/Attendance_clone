cc20c648611b206809485a13831ac8cd8ed00242
jayyedata <jayyeucla@gmail.com>
2024-03-06 07:31:06 -0800

    update mms

--- viewrecords.php
+++ viewrecords.php
@@ -51,7 +51,6 @@ $results = $crud->getAttendees();
             href="delete.php?id=<?php echo $r['attendee_id']; ?>" class="btn btn-danger">Delete</a>
 
         </td>
-
     </tr>
 <?php } ?>
 </table>
