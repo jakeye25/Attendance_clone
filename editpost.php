f3264b41eb188597894d826e961ac12a28711e18
jayyedata <jayyeucla@gmail.com>
2024-04-02 14:11:01 -0700

    update mms

--- editpost.php
+++ editpost.php
@@ -23,6 +23,7 @@ if(isset($_POST['submit'])){
     //call crud function
 
     $result = $crud->editAttendee( $id, $fname,  $lname, $dob,  $email,  $contact,  $specialty);
+
     //redirect to index.php
     if($result) {
         header("Location: viewrecords.php");
