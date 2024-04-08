ad0e558d2b45ca97f6f4a79c5c8b96401779cc0c
jayyedata <jayyeucla@gmail.com>
2024-04-08 10:56:52 -0700

    update mms

--- editpost.php
+++ editpost.php
@@ -5,7 +5,6 @@ require_once 'db/conn.php';
 if(isset($_POST['submit'])){
     //extract values from the $_POST array
     $id = $_POST['id'];
-
     $fname = $_POST['firstname'];
 
     $lname = $_POST['lastname'];
