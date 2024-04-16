babb2f09a81e600b54b0e10a59e88ec5b608af58
jayyedata <jayyeucla@gmail.com>
2024-04-16 16:36:58 -0700

    update mms

--- editpost.php
+++ editpost.php
@@ -17,6 +17,7 @@ if(isset($_POST['submit'])){
 
     //redirect to index.php
     if($result) {
+        
         header("Location: viewrecords.php");
     } else {
     include 'includes/errormessage.php';
