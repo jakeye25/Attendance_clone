dfdaae5164415bda5715a29489b821b08903500c
jayyedata <jayyeucla@gmail.com>
2024-05-08 14:01:20 -0700

    update mms

--- success.php
+++ success.php
@@ -60,7 +60,6 @@ if(isset($_POST['submit'])){
         <h5 class="card-title"><?php echo $_POST['firstname'] . ' ' . $_POST['lastname']; ?></h5>
 
         <h6 class="card-subtitle mb-2 text-muted">
-
             <?php echo $_POST['specialty']; ?>
         </h6>
 
