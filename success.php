ebc7715a229e2d623b36a95d8c42b39bfabacb67
jayyedata <jayyeucla@gmail.com>
2024-06-18 08:04:28 -0700

    update mms

--- success.php
+++ success.php
@@ -21,11 +21,12 @@ if(isset($_POST['submit'])){
     $isSuccess = $crud->insertAttendees($fname, $lname, $dob, $email, $contact, $specialty);
 
     if($isSuccess) {
-        
+
         // echo '<h1 class="text-center text-success">You Have Been Registered!</h1>';
     include 'includes/successmessage.php';
 
     } else {
+        
         // echo '<h1 class="text-center text-danger">There was an error in processing!</h1>';
     include 'includes/errormessage.php';
 
