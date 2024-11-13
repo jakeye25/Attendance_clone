41336b3b573560e2be63d7734124f77f81de5540
jayyedata <jayyeucla@gmail.com>
2024-11-13 13:56:51 -0800

    update mms

--- viewrecords.php
+++ viewrecords.php
@@ -55,6 +55,7 @@ $results = $crud->getAttendees();
             <a href="edit.php?id=<?php echo $r['attendee_id']; ?>" class="btn btn-warning">Edit</a>
             <a onclick="return confirm('Are you sure you want to delete this record?')"
             href="delete.php?id=<?php echo $r['attendee_id']; ?>" class="btn btn-danger">Delete</a>
+
         </td>
 
     </tr>
