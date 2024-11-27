04652b2e23f0242b4433803e16be9ba84689722a
jayyedata <jayyeucla@gmail.com>
2024-11-27 12:09:19 -0800

    update mms

--- viewrecords.php
+++ viewrecords.php
@@ -3,6 +3,7 @@
 $title = 'View Records';
 
 require_once 'includes/header.php';
+
 require_once 'db/conn.php';
 $results = $crud->getAttendees();
 
@@ -21,7 +22,7 @@ $results = $crud->getAttendees();
         <th>Specialty</th>
         <th>Actions</th>
     </tr>
-    
+
         <?php
         while ($r = $results->fetch(PDO::FETCH_ASSOC)) { ?>
     <tr>
