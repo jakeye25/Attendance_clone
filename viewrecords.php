13bb1c9a88afbf8dd444261e63f8d9df55016950
jayyedata <jayyeucla@gmail.com>
2024-12-02 09:54:17 -0800

    update mms

--- viewrecords.php
+++ viewrecords.php
@@ -12,12 +12,13 @@ $results = $crud->getAttendees();
 <table class="table">
 
     <tr>
+        
         <th>#</th>
 
         <th>First Name</th>
         <th>Last Name</th>
         <!-- <th>Date of Birth</th>
-         
+
         <th>Email Address</th>
 
         <th>Contact Number</th> -->
