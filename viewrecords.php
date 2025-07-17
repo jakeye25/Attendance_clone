1fd6db185df0e6468c2f3efec8d0e59a73cd8051
jayyedata <jayyeucla@gmail.com>
2025-07-17 13:40:41 -0700

    update mms

--- viewrecords.php
+++ viewrecords.php
@@ -67,7 +67,6 @@ $results = $crud->getAttendees();
         <td>
 
             <?php echo $r['name']; ?>
-
         </td>
         <td
             <a href="view.php?id=<?php echo $r['attendee_id']; ?>" class="btn btn-primary">View</a>
