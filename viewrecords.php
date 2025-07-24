a4cfeaa99dcaf627668bcc8019162d4943dc47e7
jayyedata <jayyeucla@gmail.com>
2025-07-24 12:38:34 -0700

    update mms

--- viewrecords.php
+++ viewrecords.php
@@ -61,7 +61,6 @@ $results = $crud->getAttendees();
                     ?></td>
 
         <td><?php //echo $r['contactnumber'];
-
             ?></td> -->
         <td>
             <?php echo $r['name']; ?>
