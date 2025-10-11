0ab2078efd5b43a589dd9b8357c3853f868b06da
jayyedata <jayyeucla@gmail.com>
2025-10-11 08:19:18 -0700

    update mms

--- viewrecords.php
+++ viewrecords.php
@@ -37,8 +37,9 @@ $results = $crud->getAttendees();
         while ($r = $results->fetch(PDO::FETCH_ASSOC)) { ?>
 
     <tr>
-        
+
         <td>
+            
             <?php echo $r['attendee_id']; ?>
         </td>
         <td>
